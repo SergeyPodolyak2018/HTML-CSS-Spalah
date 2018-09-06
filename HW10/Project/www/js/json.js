fe4560232c8b486492869f4d0b4089c4496d9b49
SergeyPodolyak2018 < sergey.podolyak2018@gmail.com>
2018-09-06 17:49:14 +0300

    Home work 13

---
+++ HW10/Project/www/js/json.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+const dataJson={
+	1:'1.xdfmnbdfv,mnbdmfvnd .dfnd.,fmvn df,mlvnd.,vmfn d.f,mvnd,.fvmn .d,mfvn.d,mfvn',
+	2:'2.xdfmnbdfv,mnbdmfvnd .dfnd.,fmvn df,mlvnd.,vmfn d.f,mvnd,.fvmn .d,mfvn.d,mfvn',
+	3:'3.xdfmnbdfv,mnbdmfvnd .dfnd.,fmvn df,mlvnd.,vmfn d.f,mvnd,.fvmn .d,mfvn.d,mfvn',
+	4:'4.xdfmnbdfv,mnbdmfvnd .dfnd.,fmvn df,mlvnd.,vmfn d.f,mvnd,.fvmn .d,mfvn.d,mfvn',
+	5:'5.xdfmnbdfv,mnbdmfvnd .dfnd.,fmvn df,mlvnd.,vmfn d.f,mvnd,.fvmn .d,mfvn.d,mfvn',
+	6:'6.xdfmnbdfv,mnbdmfvnd .dfnd.,fmvn df,mlvnd.,vmfn d.f,mvnd,.fvmn .d,mfvn.d,mfvn',
+	7:'7.xdfmnbdfv,mnbdmfvnd .dfnd.,fmvn df,mlvnd.,vmfn d.f,mvnd,.fvmn .d,mfvn.d,mfvn',
+
+}
